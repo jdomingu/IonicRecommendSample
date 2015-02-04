e3bb416c8834bb6d2ef8171dac66c20aa8f34808
jdomingu <jdomingu@users.noreply.github.com>
2015-02-03 22:39:09 -0800

    Added logo, beer history from Untappd for testing

--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,26 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('hopulous', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
 
 .controller('RecBeerCtrl', function($scope) {
   $scope.recbeers = [
-    { title: 'Cowiche Canyon' },
-    { title: 'Space Dust' },
-    { title: 'Boneyard RPM' },
+    { name: 'Cowiche Canyon',
+        brewery: 'Fremont Brewing' },
+    { name: 'Space Dust', 
+        brewery: 'Elysian Brewing' },
+    { name: 'Boneyard RPM',
+        brewery: 'Boneyard Brewing' },
   ];
 });
 
-//.run(function($ionicPlatform) {
-//  $ionicPlatform.ready(function() {
-//    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//    // for form inputs)
-//    if(window.cordova && window.cordova.plugins.Keyboard) {
-//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//    }
-//    if(window.StatusBar) {
-//      StatusBar.styleDefault();
-//    }
-//  });
-//})
