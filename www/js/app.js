3562179a27e94e08be41210f55c2f4e4fef9cf2b
jdomingu <jdomingu@users.noreply.github.com>
2015-02-11 00:58:00 -0800

    Move camera plugin load on device ready

--- www/js/app.js
+++ www/js/app.js
@@ -4,15 +4,18 @@ angular.module('hopulous', ['ionic', 'hopulous.controllers'])
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
+        /*
         if(window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            //When the device is ready, prepare the camera.
-            pictureSource=navigator.camera.PictureSourceType;
-            destinationType=navigator.camera.DestinationType;
         }
         if(window.StatusBar) {
             StatusBar.styleDefault();
-        }
+        }*/
+        
+        //When the device is ready, prepare the camera.
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+
         
     });
 })
